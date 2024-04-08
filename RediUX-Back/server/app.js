fac71607975b8757f8e676b401c5b0143225e3d5
Luiz-Eduardo-BL <limadudu34@gmail.com>
2024-04-08 18:18:21 -0300

    Atualização do arquivo app.js
    - Importada a biblioteca "firebase/firestore"
    - Definido o mecanismo de visualização como Pug
    - Definido o diretório de visualizações
    - Configurados cabeçalhos CORS

--- RediUX-Back/server/app.js
+++ RediUX-Back/server/app.js
@@ -1,40 +1,34 @@
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-import "firebase/firestore"
+import "firebase/firestore";
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
-import contentsRouter from './routes/contents'
-
+import contentsRouter from './routes/contents';
 
 var app = express();
 
-// view engine setup
-app.set('view engine', 'pug')
+// Definindo o mecanismo de visualização como Pug
+app.set('view engine', 'pug');
+
+// Definindo o diretório de visualizações
+const viewsDirectory = path.join(__dirname, '..', 'server', 'views');
+app.set('views', viewsDirectory);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
-const viewsDirectory = process.env.npm_config_local_prefix + "\\server\\views"
-app.set('views', viewsDirectory)
 
+// Configurando cabeçalhos CORS
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
-  })
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
