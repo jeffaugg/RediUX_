54ef48c4c5c7d898a53c64593722d6f974fc3b33
Lucas Amorim <lcsamorim503@gmail.com>
2024-04-25 15:18:13 -0300

    fix: corrected syntax error causing fields not to fetch some backend information
    - Fixed syntax error where fields 'autor' and 'descrição' were mismatched as 'author' and 'description' at some point
    - Made consistent with other fields

--- RediUX-Back/dist-server/services/contents.firebase.services.js
+++ RediUX-Back/dist-server/services/contents.firebase.services.js
@@ -60,9 +60,9 @@ var ContentServices = /*#__PURE__*/function () {
           titulo: content.data().titulo,
           tags: content.data().tags,
           midia: content.data().midia,
-          description: content.data().descricao,
+          descricao: content.data().descricao,
           link: content.data().link,
-          author: content.data().autor,
+          autor: content.data().autor,
           imgUrl: content.data().imgUrl,
           file: content.data().file
         };
