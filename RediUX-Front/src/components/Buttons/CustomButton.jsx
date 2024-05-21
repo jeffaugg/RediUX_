8a96dc009304a19d2d0642b494dd7a3e92e42844
Luiz-Eduardo-BL <limadudu34@gmail.com>
2024-05-21 14:15:51 -0300

    feat: Update CustomButton component with customizable styles
    The CustomButton component now accepts additional props for customizing its background color, hover background color, text color, and hover text color. This allows for more flexibility in styling the button to match different design requirements.

--- RediUX-Front/src/components/Buttons/CustomButton.jsx
+++ RediUX-Front/src/components/Buttons/CustomButton.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Button } from "@mui/material";
 
-const CustomButton = ({ text, href, target = "_blank", ...props }) => {
+const CustomButton = ({
+  text,
+  href,
+  target = "_blank",
+  backgroundColor = "#0C2D8A",
+  hoverBackgroundColor = "#BECBEA",
+  color = "white",
+  hoverColor = "#0C2D8A",
+  ...props
+}) => {
   return (
     <Button
       variant="outlined"
@@ -10,11 +19,12 @@ const CustomButton = ({ text, href, target = "_blank", ...props }) => {
         mt: -1,
         ml: 1,
         mb: 1,
-        color: "#0C2D8A",
-        borderColor: "#0C2D8A",
+        backgroundColor,
+        color,
+        borderColor: backgroundColor, 
         '&:hover': {
-          backgroundColor: "#0C2D8A",
-          color: "#BECBEA",
+          backgroundColor: hoverBackgroundColor,
+          color: hoverColor,
         },
         ...props,
       }}
