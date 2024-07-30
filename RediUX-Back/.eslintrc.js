3704cfec1c1f1c07bbfdadf767ec654cefc8dd5f
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-07-29 22:48:13 -0300

    feat: Add ESLint configuration files

---
+++ RediUX-Back/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard',
+    'plugin:prettier/recommended',
+    
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+}
