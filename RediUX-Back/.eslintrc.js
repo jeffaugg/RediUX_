e3e5a4340bda5718efe38d2b2076521a702393a1
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-08-02 14:32:42 -0300

    chore: Update eslint configuration and dependencies

--- RediUX-Back/.eslintrc.js
+++ RediUX-Back/.eslintrc.js
@@ -1,32 +1,25 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'standard',
-    'plugin:prettier/recommended',
-    
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["standard", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  plugins: [
-    '@typescript-eslint',
-    'prettier',
-  ],
+  plugins: ["@typescript-eslint", "prettier"],
   rules: {
-    'prettier/prettier': [
-      'error',
+    "prettier/prettier": [
+      "error",
       {
-        endOfLine: 'auto'
-      }
+        endOfLine: "auto",
+      },
     ],
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
-  }
-}
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
