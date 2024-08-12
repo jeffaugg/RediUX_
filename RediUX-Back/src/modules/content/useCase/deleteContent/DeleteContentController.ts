b37b2c2b4f57798a00ce2bcd11e9a59c2ade5232
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-08-12 10:53:49 -0300

    feat: Add DeleteContentController for deleting content

---
+++ RediUX-Back/src/modules/content/useCase/deleteContent/DeleteContentController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { DeleteContentUseCase } from "./deleteContentUseCase";
+import { container } from "tsyringe";
+
+class DeleteContentController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const deleteContentUseCase = container.resolve(DeleteContentUseCase);
+    if (!id) {
+      return res.status(400).json({ message: "ID is required" });
+    }
+
+    const idInt = parseInt(id, 10);
+
+    if (isNaN(idInt)) {
+      return res.status(400).json({ message: "ID must be a number" });
+    }
+
+    try {
+      await deleteContentUseCase.execute(idInt);
+      return res.status(200).send();
+    } catch (error) {
+      return res.status(400).json({
+        message: error.message || "Unexpected error.",
+      });
+    }
+  }
+}
+
+export { DeleteContentController };
