5a97ff95825d5235c51d4e6600da3f27909fb3af
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-08-12 10:54:36 -0300

    feat: Implement UpdateContentUseCase for updating content

---
+++ RediUX-Back/src/modules/content/useCase/updateContent/UpdateContentUseCase.ts
@@ -0,0 +1,32 @@
+import { inject, injectable } from "tsyringe";
+import { Content } from "../../infra/typeorm/entity/Content";
+import { IContentRepository } from "../../repository/interface/IContentRepository";
+
+@injectable()
+class UpdateContentUseCase {
+  constructor(
+    @inject("ContentRepository")
+    private contentRepository: IContentRepository,
+  ) {}
+
+  // this method updates the content based on the id passed as a parameter
+  async execute(
+    id: number,
+    data: {
+      title?: string;
+      autor?: string;
+      description?: string;
+      link?: string;
+      media_type?: string;
+    },
+  ): Promise<Content> {
+    const contentUpdated = await this.contentRepository.update(id, data);
+
+    if (!contentUpdated) {
+      throw new Error("Content not found");
+    }
+    return contentUpdated;
+  }
+}
+
+export { UpdateContentUseCase };
