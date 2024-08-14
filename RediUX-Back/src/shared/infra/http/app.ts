51c8f52cb4bc112bd87c18c73139026874756fad
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-08-13 22:38:11 -0300

    feat: Update error handling in app.ts

--- RediUX-Back/src/shared/infra/http/app.ts
+++ RediUX-Back/src/shared/infra/http/app.ts
@@ -1,13 +1,29 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AppDataSource } from "../../../data-source";
 import "../../container/index";
 import { routes } from "./routes";
+import { AppError } from "../../erros/AppError";
 
 const app = express();
 
 AppDataSource.initialize().then(() => {
   app.use(express.json());
   app.use(routes);
+  app.use(
+    // eslint-disable-next-line
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+      if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+          message: err.message,
+        });
+      }
+
+      return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message}`,
+      });
+    },
+  );
 });
 
 export { app };
