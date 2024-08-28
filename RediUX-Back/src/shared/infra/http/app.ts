217f0a9f5fea0d42e747eaad1aa8e2c231f79c1a
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-08-28 16:55:07 -0300

    feat: Enable CORS for the application

--- RediUX-Back/src/shared/infra/http/app.ts
+++ RediUX-Back/src/shared/infra/http/app.ts
@@ -6,9 +6,11 @@ import "../../container/index";
 import { routes } from "./routes";
 import { AppError } from "../../erros/AppError";
 import SwaggerFile from "../../../swagger.json";
+import cors from "cors";
 const app = express();
 
 AppDataSource.initialize().then(() => {
+  app.use(cors());
   app.use(express.json());
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(SwaggerFile));
   app.use(routes);
