d340c700f9c3ca8427cd88e27fbf88600e8df30f
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-02 18:34:17 -0300

    feat: add Zod schema for content validation

---
+++ RediUX-Back/src/modules/content/infra/zod/SchemaContent.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const SchemaContent = z.object({
+  title: z
+    .string()
+    .min(1, { message: "Title must be at least 1 character long" }),
+  autor: z
+    .string()
+    .min(1, { message: "Autor name must be at least 1 character long" }),
+  description: z
+    .string()
+    .max(500, { message: "Description must be at most 500 characters long" }),
+  link: z.string().url(),
+  media_type: z.enum(["livro", "artigo", "podcast", "video"]),
+  tags: z.array(
+    z.number().int().positive().min(1, { message: "Tag id must be positive" }),
+  ),
+});
