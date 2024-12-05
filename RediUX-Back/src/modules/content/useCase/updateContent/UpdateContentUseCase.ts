cc72f86ef8f09a4372d7d482ff5d89aa76e5bc93
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-04 21:58:22 -0300

    feat: refactor UpdateContentUseCase to include TagRepository and improve tag handling

--- RediUX-Back/src/modules/content/useCase/updateContent/UpdateContentUseCase.ts
+++ RediUX-Back/src/modules/content/useCase/updateContent/UpdateContentUseCase.ts
@@ -2,28 +2,49 @@ import { inject, injectable } from "tsyringe";
 import { Content } from "../../infra/typeorm/entity/Content";
 import { IContentRepository } from "../../repository/interface/IContentRepository";
 import { AppError } from "../../../../shared/erros/AppError";
-import { Tag } from "../../infra/typeorm/entity/Tag";
+import { TagRepository } from "../../repository/TagRepository";
+
+interface IRequest {
+  title: string;
+  autor: string;
+  description: string;
+  link: string;
+  media_type: string;
+  tags: number[];
+}
 
 @injectable()
 class UpdateContentUseCase {
   constructor(
     @inject("ContentRepository")
     private contentRepository: IContentRepository,
+    @inject("TagRepository")
+    private tagRepository: TagRepository,
   ) {}
 
   // this method updates the content based on the id passed as a parameter
   async execute(
     id: number,
-    data: {
-      title?: string;
-      autor?: string;
-      description?: string;
-      link?: string;
-      media_type?: string;
-      tags?: Tag[];
-    },
+    { title, autor, description, link, media_type, tags }: IRequest,
   ): Promise<Content> {
-    const contentUpdated = await this.contentRepository.update(id, data);
+    const tagEntities = await Promise.all(
+      tags.map(async (tag) => {
+        const tagById = await this.tagRepository.findById(tag);
+        if (!tagById) {
+          throw new AppError("Tag not found");
+        }
+        return tagById;
+      }),
+    );
+
+    const contentUpdated = await this.contentRepository.update(id, {
+      title,
+      autor,
+      description,
+      link,
+      media_type,
+      tags: tagEntities,
+    });
 
     if (!contentUpdated) {
       throw new AppError("Content not found");
