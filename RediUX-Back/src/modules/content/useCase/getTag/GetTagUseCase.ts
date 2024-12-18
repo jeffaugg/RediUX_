73b7ea82811d271d82fd4cb3fbc0400b544a4124
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-17 22:58:39 -0300

    feat: remove unnecessary tag existence check in GetTagUseCase

--- RediUX-Back/src/modules/content/useCase/getTag/GetTagUseCase.ts
+++ RediUX-Back/src/modules/content/useCase/getTag/GetTagUseCase.ts
@@ -1,5 +1,4 @@
 import { inject, injectable } from "tsyringe";
-import { AppError } from "../../../../shared/erros/AppError";
 import { Tag } from "../../infra/typeorm/entity/Tag";
 
 import { TagRepository } from "../../repository/TagRepository";
@@ -17,10 +16,6 @@ class GetTagUseCase {
     limit?: number;
   }): Promise<Tag[]> {
     const tag = await this.tagRepository.list(data);
-
-    if (!tag) {
-      throw new AppError("Tag not found");
-    }
     return tag;
   }
 }
