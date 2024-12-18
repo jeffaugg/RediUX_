6d668b0eca6df7cfe55db8acb20e3afc60ee52a3
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-17 22:57:25 -0300

    feat: add minimum length validation for description in SchemaContent

--- RediUX-Back/src/modules/content/infra/zod/SchemaContent.ts
+++ RediUX-Back/src/modules/content/infra/zod/SchemaContent.ts
@@ -9,6 +9,7 @@ export const SchemaContent = z.object({
     .min(1, { message: "Autor name must be at least 1 character long" }),
   description: z
     .string()
+    .min(1, { message: "Description must be at least 1 character long" })
     .max(500, { message: "Description must be at most 500 characters long" }),
   link: z.string().url(),
   media_type: z.enum(["livro", "artigo", "podcast", "video"]),
