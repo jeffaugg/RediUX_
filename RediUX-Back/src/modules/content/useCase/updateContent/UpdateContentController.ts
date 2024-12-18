b5d5a148d1817b9be84232f43762bc9fa5e27b9a
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-17 22:58:43 -0300

    feat: simplify error handling and ID parsing in UpdateContentController

--- RediUX-Back/src/modules/content/useCase/updateContent/UpdateContentController.ts
+++ RediUX-Back/src/modules/content/useCase/updateContent/UpdateContentController.ts
@@ -7,32 +7,22 @@ class UpdateContentController {
   async updateContent(req: Request, res: Response): Promise<Response> {
     const updateContentUseCase = container.resolve(UpdateContentUseCase);
 
-    try {
-      const { id } = req.params;
-      const { title, autor, description, link, media_type, tags } =
-        SchemaContent.parse(req.body);
+    const { id } = req.params;
+    const { title, autor, description, link, media_type, tags } =
+      SchemaContent.parse(req.body);
 
-      const idInt = parseInt(id, 10);
-      if (isNaN(idInt)) {
-        return res.status(400).json({ message: "ID must be a number" });
-      }
+    const idInt = Number(id);
 
-      const updatedContent = await updateContentUseCase.execute(idInt, {
-        title,
-        autor,
-        description,
-        link,
-        media_type,
-        tags,
-      });
+    const updatedContent = await updateContentUseCase.execute(idInt, {
+      title,
+      autor,
+      description,
+      link,
+      media_type,
+      tags,
+    });
 
-      return res.status(200).json(updatedContent);
-    } catch (error: any) {
-      console.error(error);
-      return res
-        .status(500)
-        .json({ message: error.message || "Internal Server Error", error });
-    }
+    return res.status(200).json(updatedContent);
   }
 }
 
